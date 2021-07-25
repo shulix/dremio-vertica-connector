8a1a33f0ace8b8ac704a2d5d44112fa2ac739095
root <root@srv-01-05-105.iad1.trmr.io>
2021-07-25 11:18:02 -0400

    init commit

--- src/main/java/com/dremio/exec/store/jdbc/conf/VerticaConf.java
+++ src/main/java/com/dremio/exec/store/jdbc/conf/VerticaConf.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2018 Dremio Corporation
+ * Copyright (C) 2017-2019 Dremio Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,60 +13,51 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.dremio.exec.store.jdbc.conf;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import org.hibernate.validator.constraints.NotBlank;
-
+import com.dremio.exec.store.jdbc.*;
+import com.dremio.options.OptionManager;
+import com.dremio.security.CredentialsService;
+import org.apache.log4j.Logger;
 import com.dremio.exec.catalog.conf.DisplayMetadata;
 import com.dremio.exec.catalog.conf.NotMetadataImpacting;
 import com.dremio.exec.catalog.conf.Secret;
 import com.dremio.exec.catalog.conf.SourceType;
-import com.dremio.exec.server.SabotContext;
-import com.dremio.exec.store.jdbc.CloseableDataSource;
-import com.dremio.exec.store.jdbc.DataSources;
-import com.dremio.exec.store.jdbc.JdbcStoragePlugin;
-import com.dremio.exec.store.jdbc.JdbcStoragePlugin.Config;
+import com.dremio.exec.store.jdbc.JdbcPluginConfig;
 import com.dremio.exec.store.jdbc.dialect.arp.ArpDialect;
+import com.dremio.exec.store.jdbc.dialect.arp.ArpYaml;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.annotations.VisibleForTesting;
-
 import io.protostuff.Tag;
 
-/**
- * Configuration for vertica sources.
- */
-@SourceType(value = "verticaARP", label = "vertica")
+@SourceType(value = "VERTICA", label = "Vertica", uiConfig = "vertica-layout.json", externalQuerySupported = true)
 public class VerticaConf extends AbstractArpConf<VerticaConf> {
-  private static final String ARP_FILENAME = "arp/implementation/vertica-arp.yaml";
-  private static final ArpDialect ARP_DIALECT =
-      AbstractArpConf.loadArpFile(ARP_FILENAME, (ArpDialect::new));
-  private static final String DRIVER = "com.vertica.jdbc.Driver";
+    private static final String ARP_FILENAME = "arp/implementation/vertica-arp.yaml";
+    private static final ArpDialect ARP_DIALECT =
+            AbstractArpConf.loadArpFile(ARP_FILENAME, (ArpDialect::new));
+    private static final String DRIVER = "com.vertica.jdbc.Driver";
 
-  
-  @NotBlank
   @Tag(1)
   @DisplayMetadata(label = "Host")
   public String host;
   
     
-  @NotBlank
   @Tag(2)
   @DisplayMetadata(label = "Port")
   public String port;
   
-  @NotBlank
   @Tag(3)
   @DisplayMetadata(label = "Database")
   public String database;
   
-  @NotBlank
   @Tag(4)
   @DisplayMetadata(label = "Username")
   public String user;
 
 
-  @NotBlank
   @Tag(5)
   @Secret
   @DisplayMetadata(label = "Password")
@@ -76,9 +67,8 @@ public class VerticaConf extends AbstractArpConf<VerticaConf> {
   @Tag(6)
   @DisplayMetadata(label = "Record fetch size")
   @NotMetadataImpacting
+  
   public int fetchSize = 200;
-
-
 
   @VisibleForTesting
   public String toJdbcConnectionString() {
@@ -89,30 +79,35 @@ public class VerticaConf extends AbstractArpConf<VerticaConf> {
     //return String.format("jdbc:vertica://192.168.0.23:5433/vmart?user=dbadmin&password=password");
   }
 
-  @Override
-  @VisibleForTesting
-  public Config toPluginConfig(SabotContext context) {
-    return JdbcStoragePlugin.Config.newBuilder()
-        .withDialect(getDialect())
-        .withFetchSize(fetchSize)
-        .withDatasourceFactory(this::newDataSource)
-        .clearHiddenSchemas()
-        //.addHiddenSchema("SYSTEM")
-        .build();
+
+
+    @Override
+    @VisibleForTesting
+    public JdbcPluginConfig buildPluginConfig(
+            JdbcPluginConfig.Builder configBuilder,
+            CredentialsService credentialsService,
+            OptionManager optionManager
+    ){
+        return configBuilder.withDialect(getDialect())
+            .withFetchSize(fetchSize)
+            .withDatasourceFactory(this::newDataSource)
+            .clearHiddenSchemas()
+            .build();
+    }
+
+    private CloseableDataSource newDataSource() {
+        return DataSources.newGenericConnectionPoolDataSource(DRIVER,
+            toJdbcConnectionString(), user, password, null, DataSources.CommitMode.DRIVER_SPECIFIED_COMMIT_MODE, 10 , 300);
   }
 
-  private CloseableDataSource newDataSource() {
-    return DataSources.newGenericConnectionPoolDataSource(DRIVER,
-      toJdbcConnectionString(), user, password, null, DataSources.CommitMode.DRIVER_SPECIFIED_COMMIT_MODE);
-  }
 
-  @Override
-  public ArpDialect getDialect() {
-    return ARP_DIALECT;
-  }
+    @Override
+    public ArpDialect getDialect() {
+        return ARP_DIALECT;
+    }
 
-  @VisibleForTesting
-  public static ArpDialect getDialectSingleton() {
-    return ARP_DIALECT;
-  }
+    @VisibleForTesting
+    public static ArpDialect getDialectSingleton() {
+        return ARP_DIALECT;
+    }
 }
